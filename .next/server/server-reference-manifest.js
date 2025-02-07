83343a303f37c4f3e91f4f940d1dc8f1cb756946
Shamal Khalid N P <shamalkhalidnp@gmail.com>
2025-02-07 11:18:51 +0530

    Added Manali package

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oGXdZyVsoIHgZUnecs2o2LnnWgaY5ONvMREddu1HdwU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aIqTiPxMtVUAe93Yh0L18Sadak0qgVDiQcfGbyDcZXQ=\"\n}"
