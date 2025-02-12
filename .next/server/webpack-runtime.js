17a07e8c342506b85b5e71d41f66ec200c0e9f8a
Shamal Khalid N P <shamalkhalidnp@gmail.com>
2025-02-13 01:39:32 +0530

    Added minor changes

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("bf3f0d36cfca0dc4")
+/******/ 		__webpack_require__.h = () => ("2bb5e567b4434dc5")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
---
+++ src/app/services/page.js
@@ -0,0 +1,194 @@
+"use client";
+import Breadcrumb from "@/components/common/Breadcrumb";
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Breadcrumb pagename="Services" pagetitle="Services" />
+      <div className="package-grid-with-sidebar-section pt-120 mb-40">
+        <div className="container">
+          <div className="row g-lg-4 ">
+            <div className="col-lg-12">
+              <div className="list-grid-product-wrap mb-70">
+                <div className="row gy-4">
+                  <div className="col-lg-4 col-md-6 item">
+                    <div className="package-card">
+                      <div className="package-card-img-wrap">
+                        <Link href="/visa" className="card-img">
+                          <img
+                            src="/assets/img/home1/package-card-img1.png"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="package-card-content">
+                        <div
+                          className="card-content-top"
+                          style={{ textAlign: "center!important" }}
+                        >
+                          <h5>
+                            <Link href="/visa">Visit Visas</Link>
+                          </h5>
+                        </div>
+                        <div className="card-content-bottom">
+                          <Link href="/visa" className="primary-btn6">
+                            Check it Out
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 item">
+                    <div className="package-card">
+                      <div className="package-card-img-wrap">
+                        <Link href="/contact" className="card-img">
+                          <img
+                            src="/assets/img/home1/package-card-img1.png"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="package-card-content">
+                        <div
+                          className="card-content-top"
+                          style={{ textAlign: "center!important" }}
+                        >
+                          <h5>
+                            <Link href="/contact">Recruitment</Link>
+                          </h5>
+                        </div>
+                        <div className="card-content-bottom">
+                          <Link href="/contact" className="primary-btn6">
+                            Contact Us
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 item">
+                    <div className="package-card">
+                      <div className="package-card-img-wrap">
+                        <Link href="/contact" className="card-img">
+                          <img
+                            src="/assets/img/home1/package-card-img1.png"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="package-card-content">
+                        <div
+                          className="card-content-top"
+                          style={{ textAlign: "center!important" }}
+                        >
+                          <h5>
+                            <Link href="/contact">
+                              Umrah &amp; Hajj booking
+                            </Link>
+                          </h5>
+                        </div>
+                        <div className="card-content-bottom">
+                          <Link href="/contact" className="primary-btn6">
+                            Contact Us
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 item">
+                    <div className="package-card">
+                      <div className="package-card-img-wrap">
+                        <Link href="/contact" className="card-img">
+                          <img
+                            src="/assets/img/home1/package-card-img1.png"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="package-card-content">
+                        <div
+                          className="card-content-top"
+                          style={{ textAlign: "center!important" }}
+                        >
+                          <h5>
+                            <Link href="/contact">Document Attestation</Link>
+                          </h5>
+                        </div>
+                        <div className="card-content-bottom">
+                          <Link href="/contact" className="primary-btn6">
+                            Contact Us
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 item">
+                    <div className="package-card">
+                      <div className="package-card-img-wrap">
+                        <Link href="/contact" className="card-img">
+                          <img
+                            src="/assets/img/home1/package-card-img1.png"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="package-card-content">
+                        <div
+                          className="card-content-top"
+                          style={{ textAlign: "center!important" }}
+                        >
+                          <h5>
+                            <Link href="/contact">Flight Tickets</Link>
+                          </h5>
+                        </div>
+                        <div className="card-content-bottom">
+                          <Link href="/contact" className="primary-btn6">
+                            Contact Us
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4 col-md-6 item">
+                    <div className="package-card">
+                      <div className="package-card-img-wrap">
+                        <Link href="/contact" className="card-img">
+                          <img
+                            src="/assets/img/home1/package-card-img1.png"
+                            alt=""
+                          />
+                        </Link>
+                      </div>
+                      <div className="package-card-content">
+                        <div
+                          className="card-content-top"
+                          style={{ textAlign: "center!important" }}
+                        >
+                          <h5>
+                            <Link href="/contact">Study Abroad</Link>
+                          </h5>
+                        </div>
+                        <div className="card-content-bottom">
+                          <Link href="/contact" className="primary-btn6">
+                            Contact Us
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default page;
